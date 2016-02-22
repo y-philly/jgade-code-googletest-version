54d56305fdd79be3ac978d6b86b83cfb6aaabc8f
Yasuhiro SHIMIZU <yasuhiro.phil81@gmail.com>
2016-02-22 14:24:44 +0900

    passed all tests except need spy.

--- test/util/CircularBuffer/CircularBufferTest.cpp
+++ test/util/CircularBuffer/CircularBufferTest.cpp
@@ -23,7 +23,9 @@
 
 using ::testing::Eq;
 
-class CircularBufferTest : public ::testing::Test {
+namespace CircularBufferTest {
+
+class CircularBuffer : public ::testing::Test {
 protected:
     virtual void SetUp()
     {
@@ -42,151 +44,150 @@ protected:
             CircularBuffer_Put(buffer, i + seed);
     }
 
-    CircularBuffer buffer;
+    ::CircularBuffer buffer;
 };
 
-TEST_F(CircularBufferTest, Create)
-{
+TEST_F(CircularBuffer, create) {}
 
+TEST_F(CircularBuffer, empty_after_creation)
+{
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(true));
 }
 
-// TEST(CircularBuffer, EmptyAfterCreation)
-// {
-//     CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
-// }
-//
-// TEST(CircularBuffer, NotFullAfterCreation)
-// {
-//     CHECK_FALSE(CircularBuffer_IsFull(buffer));
-// }
-//
-// TEST(CircularBuffer, NotEmpty)
-// {
-//     CircularBuffer_Put(buffer, 10046);
-//     CHECK_FALSE(CircularBuffer_IsEmpty(buffer));
-// }
-//
-// TEST(CircularBuffer, NotEmptyThenEmpty)
-// {
-//     CircularBuffer_Put(buffer, 4567);
-//     CHECK_FALSE(CircularBuffer_IsEmpty(buffer));
-//     CircularBuffer_Get(buffer);
-//     CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
-// }
-//
-// TEST(CircularBuffer, GetPutOneValue)
-// {
-//     CircularBuffer_Put(buffer, 4567);
-//     LONGS_EQUAL(4567, CircularBuffer_Get(buffer));
-// }
-//
-// TEST(CircularBuffer, GetPutAFew)
-// {
-//     CircularBuffer_Put(buffer, 1);
-//     CircularBuffer_Put(buffer, 2);
-//     CircularBuffer_Put(buffer, 3);
-//     LONGS_EQUAL(1, CircularBuffer_Get(buffer));
-//     LONGS_EQUAL(2, CircularBuffer_Get(buffer));
-//     LONGS_EQUAL(3, CircularBuffer_Get(buffer));
-// }
-//
-// TEST(CircularBuffer, Capacity)
-// {
-//     CircularBuffer b = CircularBuffer_Create(2);
-//     LONGS_EQUAL(2, CircularBuffer_Capacity(b));
-//     CircularBuffer_Destroy(b);
-// }
-//
-//
-// TEST(CircularBuffer, IsFull)
-// {
-//     for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
-//         CircularBuffer_Put(buffer, i+100);
-//
-//     CHECK_TRUE(CircularBuffer_IsFull(buffer));
-// }
-//
-// TEST(CircularBuffer, EmptyToFullToEmpty)
-// {
-//     for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
-//         CircularBuffer_Put(buffer, i+100);
-//
-//     CHECK_TRUE(CircularBuffer_IsFull(buffer));
-//
-//     for (int j = 0; j < CircularBuffer_Capacity(buffer); j++)
-//         LONGS_EQUAL(j+100, CircularBuffer_Get(buffer));
-//
-//     CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
-//     CHECK_FALSE(CircularBuffer_IsFull(buffer));
-// }
-//
-// TEST(CircularBuffer, WrapAround)
-// {\
-//     int capacity = CircularBuffer_Capacity(buffer);
-//     for (int i = 0; i < capacity; i++)
-//         CircularBuffer_Put(buffer, i+100);
-//
-//     CHECK_TRUE(CircularBuffer_IsFull(buffer));
-//     LONGS_EQUAL(100, CircularBuffer_Get(buffer));
-//     CHECK_FALSE(CircularBuffer_IsFull(buffer));
-//     CircularBuffer_Put(buffer, 1000);
-//     CHECK_TRUE(CircularBuffer_IsFull(buffer));
-//
-//     for (int j = 1; j < capacity; j++)
-//         LONGS_EQUAL(j+100, CircularBuffer_Get(buffer));
-//
-//     LONGS_EQUAL(1000, CircularBuffer_Get(buffer));
-//     CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
-// }
-//
-// TEST(CircularBuffer, PutToFullThrows)
-// {
-//     putManyInTheBuffer(900, CircularBuffer_Capacity(buffer));
-//     CHECK_FALSE(CircularBuffer_Put(buffer, 9999));
-// }
-//
-// TEST(CircularBuffer, PutToFullDoesNotDamageContents)
-// {
-//     putManyInTheBuffer(900, CircularBuffer_Capacity(buffer));
-//
-//         CircularBuffer_Put(buffer, 9999);
-//
-//     for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
-//         LONGS_EQUAL(i+900, CircularBuffer_Get(buffer));
-//
-//     CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
-// }
-//
-// TEST(CircularBuffer, GetFromEmptyReturns0)
-// {
-//     LONGS_EQUAL(0, CircularBuffer_Get(buffer));
-// }
-//
-//
-// //START: PrintSetupAndTearDownDuplication
-// TEST(CircularBuffer, PrintEmpty)
-// {
-//     const char* expectedOutput = "Circular buffer content:\n<>\n";
-//     FormatOutputSpy_Create(100);
-//     UT_PTR_SET(FormatOutput, FormatOutputSpy);
-//
-//     CircularBuffer_Print(buffer);
-//
-//     STRCMP_EQUAL(expectedOutput, FormatOutputSpy_GetOutput());
-//     FormatOutputSpy_Destroy();
-// }
-//
-// TEST(CircularBuffer, PrintAfterOneIsPut)
-// {
-//     const char* expectedOutput = "Circular buffer content:\n<17>\n";
-//     FormatOutputSpy_Create(100);
-//     UT_PTR_SET(FormatOutput, FormatOutputSpy);
-//
-//     CircularBuffer_Put(buffer, 17);
-//     CircularBuffer_Print(buffer);
-//
-//     STRCMP_EQUAL(expectedOutput, FormatOutputSpy_GetOutput());
-//     FormatOutputSpy_Destroy();
-// }
-// //END: PrintSetupAndTearDownDuplication
-//
+TEST_F(CircularBuffer, not_full_after_creation)
+{
+    ASSERT_THAT(CircularBuffer_IsFull(buffer), Eq(false));
+}
+
+TEST_F(CircularBuffer, not_empty)
+{
+    CircularBuffer_Put(buffer, 10046);
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(false));
+}
+
+TEST_F(CircularBuffer, not_empty_then_empty)
+{
+    CircularBuffer_Put(buffer, 4567);
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(false));
+    CircularBuffer_Get(buffer);
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(true));
+}
+
+TEST_F(CircularBuffer, get_put_one_value)
+{
+    CircularBuffer_Put(buffer, 4567);
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(4567));
+}
+
+TEST_F(CircularBuffer, get_put_a_few)
+{
+    CircularBuffer_Put(buffer, 1);
+    CircularBuffer_Put(buffer, 2);
+    CircularBuffer_Put(buffer, 3);
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(1));
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(2));
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(3));
+}
+
+TEST_F(CircularBuffer, capacity)
+{
+    ::CircularBuffer b = CircularBuffer_Create(2);
+    ASSERT_THAT(CircularBuffer_Capacity(b), Eq(2));
+    CircularBuffer_Destroy(b);
+}
+
+
+TEST_F(CircularBuffer, is_full)
+{
+    for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
+        CircularBuffer_Put(buffer, i + 100);
+
+    ASSERT_THAT(CircularBuffer_IsFull(buffer), Eq(true));
+}
+
+TEST_F(CircularBuffer, empty_to_full_to_empty)
+{
+    for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
+        CircularBuffer_Put(buffer, i + 100);
+
+    ASSERT_THAT(CircularBuffer_IsFull(buffer), Eq(true));
+
+    for (int j = 0; j < CircularBuffer_Capacity(buffer); j++)
+        ASSERT_THAT(CircularBuffer_Get(buffer), Eq(j + 100));
+
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(true));
+    ASSERT_THAT(CircularBuffer_IsFull(buffer),  Eq(false));
+}
+
+TEST_F(CircularBuffer, wrap_around)
+{
+    int capacity = CircularBuffer_Capacity(buffer);
+    for (int i = 0; i < capacity; i++)
+        CircularBuffer_Put(buffer, i + 100);
+
+    ASSERT_THAT(CircularBuffer_IsFull(buffer),  Eq(true));
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(100));
+    ASSERT_THAT(CircularBuffer_IsFull(buffer),  Eq(false));
+    CircularBuffer_Put(buffer, 1000);
+    ASSERT_THAT(CircularBuffer_IsFull(buffer),  Eq(true));
+
+    for (int j = 1; j < capacity; j++)
+        ASSERT_THAT(CircularBuffer_Get(buffer), Eq(j + 100));
+
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(1000));
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(true));
+}
+
+TEST_F(CircularBuffer, put_to_full_throws)
+{
+    PutManyInTheBuffer(900, CircularBuffer_Capacity(buffer));
+    ASSERT_THAT(CircularBuffer_Put(buffer, 9999), Eq(false));
+}
+
+TEST_F(CircularBuffer, put_to_full_does_not_damage_contents)
+{
+    PutManyInTheBuffer(900, CircularBuffer_Capacity(buffer));
+    CircularBuffer_Put(buffer, 9999);
+
+    for (int i = 0; i < CircularBuffer_Capacity(buffer); i++)
+        ASSERT_THAT(CircularBuffer_Get(buffer), Eq(i + 900));
+
+    ASSERT_THAT(CircularBuffer_IsEmpty(buffer), Eq(true));
+}
+
+TEST_F(CircularBuffer, get_from_empty_returns0)
+{
+    ASSERT_THAT(CircularBuffer_Get(buffer), Eq(0));
+}
+
+#if 0
+
+//START: PrintSetupAndTearDownDuplication
+TEST_F(CircularBuffer, PrintEmpty)
+{
+    const char* expectedOutput = "Circular buffer content:\n<>\n";
+    FormatOutputSpy_Create(100);
+    UT_PTR_SET(FormatOutput, FormatOutputSpy);
+
+    CircularBuffer_Print(buffer);
+
+    STRCMP_EQUAL(expectedOutput, FormatOutputSpy_GetOutput());
+    FormatOutputSpy_Destroy();
+}
+
+TEST_F(CircularBuffer, PrintAfterOneIsPut)
+{
+    const char* expectedOutput = "Circular buffer content:\n<17>\n";
+    FormatOutputSpy_Create(100);
+    UT_PTR_SET(FormatOutput, FormatOutputSpy);
+
+    CircularBuffer_Put(buffer, 17);
+    CircularBuffer_Print(buffer);
+
+    STRCMP_EQUAL(expectedOutput, FormatOutputSpy_GetOutput());
+    FormatOutputSpy_Destroy();
+}
+
+#endif
+
+} // namespace CircularBufferTest
